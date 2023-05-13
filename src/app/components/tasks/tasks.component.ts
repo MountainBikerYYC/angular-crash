1f2260df9baae5cd905c3f837ad321f4f548e86c
MountainBikerYYC <ywang9696@gmail.com>
2023-05-13 01:22:50 -0600

    Created service class

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { TASKS } from '../../mock-tasks';
 import { Task } from '../../Task';
+import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
-    tasks: Task[] = TASKS
+    tasks: Task[] = [];
+
+    constructor(private taskService: TaskService){
+
+    }
+
+    ngOnInit(): void{
+        this.tasks= this.taskService.getTasks();
+
+    }
 
 }
